a518e2371387dd72b0ec6f7bdb916ca9e272ec65
Travis Rich <travis.s.rich@gmail.com>
2015-10-22 17:37:40 -0400

    Fixing css test 3

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: [strip.loader('debug'), 'babel']},
+      { test: /\.js$/, exclude: /(node_modules|bootstrap-sass\.config)/, loaders: [strip.loader('debug'), 'babel']},
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap=true&sourceMapContents=true') },
       { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true') },
